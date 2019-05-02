154b8f3e07b06800e1f89578bea6149fabe03ed2
Alvin Chan <aschan@gmail.com>
2019-05-02 11:20:27 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ]
+}
