4e18b53f024d2bd3d0d732bbd33554547c7e887b
Alvin Chan <aschan@gmail.com>
2020-06-17 17:27:26 -0700

    Set React version to `detect` for `eslint-plugin-react`

--- react.js
+++ react.js
@@ -3,4 +3,5 @@ const config = require('./index.js')
 module.exports = {
   ...config,
   extends: [...config.extends, 'plugin:react/recommended', 'prettier/react'],
+  settings: { react: { version: 'detect' } },
 }
